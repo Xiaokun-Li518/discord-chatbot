d3d9d9fc2d7bd8666063b160147d52185b68adb2
li,xiaokun <leeli@iu.edu>
2023-03-23 19:54:20 -0400

    added joke command

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 import discord
 import os
 from pyrandmeme import *
+from pyjokes import get_joke
+
+from memeMaker import makeMeme
 
 
 from app.chatgpt_ai.openai import chatgpt_response, dalle_response
@@ -11,7 +14,7 @@ load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
 
 class MyClient(discord.Client):
-    conversation = [{'role': 'system', 'content': 'You are an advanced assistant. Your job is to answer the question as short and concise as possible.'}]
+    conversation = [{'role': 'system', 'content': 'answer the question as short and concise as possible.'}]
     async def on_ready(self):
         print ("Succcessfull logged in as: ", self.user)
 
@@ -20,7 +23,7 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
         command, user_message = None, None
-        for text in ['!ai', '!bot', '!gpt', '!session', '!image', '!meme']:
+        for text in ['!ai', '!bot', '!gpt', '!session', '!image', '!meme', '!joke']:
             if message.content.startswith(text):
                 arr = message.content.split(' ');
                 command=arr[0]
@@ -32,7 +35,7 @@ class MyClient(discord.Client):
             MyClient.conversation = chatgpt_response(messages=MyClient.conversation)
             await message.channel.send(f"{MyClient.conversation[-1]['content']}")
         elif command == '!session':
-            MyClient.conversation = [{'role': 'system', 'content': 'You are an advanced assistant. Your job is to answer the question as short and concise as possible.'}]
+            MyClient.conversation = [{'role': 'system', 'content': 'answer the question as short and concise as possible.'}]
             MyClient.conversation.append({'role':'user', 'content':user_message})
             MyClient.conversation = chatgpt_response(messages=MyClient.conversation)
             await message.channel.send(f"{MyClient.conversation[-1]['content']}")
@@ -41,6 +44,8 @@ class MyClient(discord.Client):
             await message.channel.send(f"{dalle_res}")
         elif command == '!meme':
             await message.channel.send(embed=await pyrandmeme())
+        elif command == '!joke':
+            await message.channel.send(get_joke())
 
 
 intents = discord.Intents.default()
