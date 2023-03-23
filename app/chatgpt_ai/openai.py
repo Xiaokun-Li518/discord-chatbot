3fb9ae59399433e8d039ca714b0809a503d3b36a
li,xiaokun <leeli@iu.edu>
2023-03-23 08:56:53 -0400

    return memes if try fails

--- app/chatgpt_ai/openai.py
+++ app/chatgpt_ai/openai.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import openai
 import os
+import random
+
 
 load_dotenv()
 
@@ -26,10 +28,21 @@ def dalle_response(prompt):
         response = openai.Image.create(
             prompt=prompt,
             n=1,
-            size="512x512"
+            size="1024*1024"
         ) 
     except:
-        return "Something went wrong, Please try again later"
+        im = ['https://media.tenor.com/3p5KHNlNpVYAAAAC/flip-off.gif', 
+              'https://media.tenor.com/tP0M0uP3apQAAAAd/no-fuck-off-fuck-off.gif',
+              'https://media.tenor.com/Jns9XX2xpKsAAAAM/fuck-fuck-off.gif',
+              'https://media.tenor.com/FzEjMu4P5fIAAAAM/middle-finger-fuck-off.gif', 
+              'https://media.tenor.com/8gpOOFO31fgAAAAM/fuck-off-go-away.gif',
+              'https://media.makeameme.org/created/nope-try-again-845dad4218.jpg',
+              'https://media.tenor.com/Ovl6WVOhOSMAAAAM/try-again.gif',
+              'https://media.tenor.com/1OK2Woyhz-AAAAAM/try-again-never-again.gif',
+              'https://media.tenor.com/-wsLV6WhY-QAAAAC/try-again-keegan-michael-key.gif'
+              ]
+        random_int = random.randint(0, 8)
+        return im[random_int]
 
     response_dict = response.get("data")
     return response_dict[0].url
